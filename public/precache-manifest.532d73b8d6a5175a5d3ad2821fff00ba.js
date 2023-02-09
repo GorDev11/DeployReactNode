7ec65b3550ea5b8c39447330bf6c3a205e1d28f6
GorDev11 <harutyunyangor1995@gmail.com>
2023-02-10 01:02:29 +0400

    Initial commit

---
+++ public/precache-manifest.532d73b8d6a5175a5d3ad2821fff00ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa4b54e2810ebbb78d964b270c08d80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63807927280126bb6ca8",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "63807927280126bb6ca8",
+    "url": "/static/js/2.abd6da21.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.abd6da21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4dd708d3bc0dda486d6",
+    "url": "/static/js/main.fbc7e810.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static('public'))
+
+
+io.on('connection', socket => {
+    console.log("connection");
+  const id = socket.handshake.query.id
+  socket.join(id)
+
+  socket.on('send-message', ({ recipients, text }) => {
+    recipients.forEach(recipient => {
+      const newRecipients = recipients.filter(r => r !== recipient)
+      newRecipients.push(id)
+      socket.broadcast.to(recipient).emit('receive-message', {
+        recipients: newRecipients, sender: id, text
+      })
+    })
+  })
+})
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
